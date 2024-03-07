86fd5a40e33953a517dde66e70a3bf1084c3264c
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-03-06 23:40:02 -0500

    Fixed issues with Head element and fonts missing properties, and title not at page level

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />{" "}
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat&display=optional"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Page Title</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
-        ></link>
+        <title>Star Ayisyen</title>
       </Head>
       <Layout></Layout>
     </>
