028f92755ebaae7947df94bced663137f5b6e8cb
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-03-06 23:31:35 -0500

    Added the search api endpoint

---
+++ pages/api/search/[query].ts
@@ -0,0 +1,57 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDb from "@/lib/mongo/dbConnect";
+import MStar from "@/lib/mongo/models/star";
+import { IStar } from "@/utils/interfaces";
+
+connectDb();
+
+interface Data {
+  success: boolean;
+  data?: IStar[];
+  msg?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+    // Get the query nad method of the request
+    const { method, query } = req;
+
+    // Only process for a GET request
+    if (method === "GET") { 
+      try {
+        // clean the query passed
+        const term = String(query).toLowerCase().trim();
+
+        // Find the stars that matched the term passed
+        const stars = await MStar.find().where("starName").regex(term);
+
+        // construct response  to send back to client
+        const response = {
+          success: true,
+          data: stars
+        };
+      
+        /** Send back the response with status code and data */
+        return res.status(200).json(response);
+
+      } catch (error) {
+        console.log(`Error processing API request : ${error}`);
+
+        // construct response  to send back to client
+        const response = {
+          success: true,
+          msg: "Error processing API request"
+        };
+
+        /** Return an error message along with the HTTP status code*/
+        return res.status(500).send(response);
+      }
+    }  
+    // Any other HTTP Method is  not supported
+    else {
+      return res.status(405).end();
+    }
+};
