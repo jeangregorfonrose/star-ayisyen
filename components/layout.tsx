37639e698eb80b6be67b8eaf9f42bc58e04acfca
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-03-09 17:51:27 -0500

    Fixed layout issue

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,15 @@
 import NavBar from "./navBar";
 import Footer from "./footer";
+import Banner from "./banner";
+import StarList from "./starList";
 
 export default function Layout({ children } : any) {
   return (
     <>
       <div className="container">
         <NavBar />
-        <main>{children}</main>
-        <Footer/>
+        {children}
+        <Footer />
       </div>
     </>
   );
