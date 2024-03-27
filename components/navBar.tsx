469239420c779e04caff41dd63417968c539c946
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-03-26 23:30:02 -0400

    Added signout button and functionality

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,9 +1,20 @@
+import { useRouter } from "next/router";
 import styles from "../styles/NavBar.module.css";
+import { signOut } from "next-auth/react";
 
 export default function NavBar() {
+  const router = useRouter()
+
+  // get path
+  const currentPath = router.pathname
+
   return (
     <nav className={styles.nav}>
       <h1>Star Ayisyen</h1>
+      {
+        currentPath == "/admin/dashboard" &&
+        <button onClick={() => signOut({callbackUrl: "/admin"})}>logout</button>
+      }
     </nav>
   );
 }
