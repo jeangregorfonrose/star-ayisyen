ec7193dc00bd2243b6b4f57620e13852928ff9ff
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-04-10 22:28:38 -0400

    Implemented the DELETE hadnler request and also made some fix for the POST request

--- pages/api/star/index.tsx
+++ pages/api/star/index.tsx
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import connectDb from "@/lib/mongo/dbConnect";
 import MStar from "@/lib/mongo/models/star";
-import { IResponse } from "@/utils/interfaces";
+import { IResponse, IStar } from "@/utils/interfaces";
 
 connectDb();
 
@@ -9,70 +9,78 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    switch(req.method){
-        // case 'GET':
-        //     try{
-        //         const stars = await MStar.find({});
-        //         return res.status(200).json(stars);
-        //     }catch(err){
-        //         console.log(err);
-        //         return res.status(400).send('Error getting stars');
-        //     }
-        // Add a new star to the database
-        case 'POST':
-            // Response to return
-            let response : IResponse = {
-                success: false
-            };
+  // Response to return
+  let response: IResponse = {
+    success: false,
+  };
 
-            if(!req.body) {
-                response.message = "Missing Data";
-                return res.status(400).send(response);
-            }
+  switch (req.method) {
+    // case 'GET':
+    //     try{
+    //         const stars = await MStar.find({});
+    //         return res.status(200).json(stars);
+    //     }catch(err){
+    //         console.log(err);
+    //         return res.status(400).send('Error getting stars');
+    //     }
+    // Add a new star to the database
+    case "POST": {
+      if (!req.body) {
+        response.message = "Missing Data";
+        return res.status(400).send(response);
+      }
 
-            // get body
-            let data = req.body;
-            
-            // check for required fields
-            const missingFields = ["starName"].filter((field) => !data[field]);
-            if (missingFields.length >  0) {
-                
-                return res.status(400).send(`Missing ${missingFields.join(", ")}.`);
-            }
+      // get body
+      let data = req.body;
 
-            // create and save a new Star instance
-            const star = new MStar(data);
-            try {
-                await star.save();
-                response.message = `Successfully added ${data.starName}`;
-                response.success= true;
-                response.data = star;
-                return res.status(201).send(response);
-            } catch (error) {
-                response.message = "Server Error";
-                return res.status(500).send(response);
-            }
-        // Update an existing star in the db
-        /*
-        case 'PUT':
-            break;
-        */
-        
-        // sDelete a star by its id
-        // case 'DELETE':
-        //     const id = req.query.id as string;
-        //     if (!MStar.isValidId(id)) {
-        //       return res.status(400).send('Invalid Star ID');
-        //     }
-    
-        //     try {
-        //       await MStar.deleteOne({_id: id});
-        //       return res.status(200).send('Deleted the star');
-        //     } catch (e) {
-        //       console.error(e);
-        //       return res.status(500).send('Server Error');
-        //     }
-      default:
-          return res.status(405).send('Method not supported');
+      // check for required fields
+      const missingFields = ["starName"].filter((field) => !data[field]);
+      if (missingFields.length > 0) {
+        return res.status(400).send(`Missing ${missingFields.join(", ")}.`);
+      }
+
+      // create and save a new Star instance
+      const star = new MStar(data);
+      try {
+        const newStar = await star.save();
+
+        if (newStar === star) {
+          response.message = `Successfully added ${data.starName}`;
+          response.success = true;
+          response.data = star;
+          return res.status(201).send(response);
+        } else {
+          throw new Error("Save failed");
+        }
+      } catch (error) {
+        response.message = "Server Error";
+        return res.status(500).send(response);
+      }
+    }
+    case "DELETE": {
+      if (!req.query.id) {
+        response.message = "Missing Data";
+        return res.status(400).send(response);
+      }
+
+      // get ID from request body
+      const id = req.query.id as string;
+
+      try {
+        // Find by ID and delete
+        const deletedStar = await MStar.findByIdAndDelete(id);
+        response.success = true;
+        response.message = "Record deleted successfully!";
+        response.data = deletedStar.toJSON();
+        return res.status(200).send(response);
+      } catch (err) {
+        response.message = "Error deleting  record.";
+        response.success = false;
+        console.log(err);
+        return res.status(400).send(response);
+      }
+    }
+    default:
+      return res.status(405).send("Method not supported");
   }
-};
+}
