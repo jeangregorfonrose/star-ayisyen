612462f03bd393072b83677fd30154f28ed4a195
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-06-29 01:10:06 -0400

    Added list of occupations

---
+++ utils/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * This file contains all the contants of the application
+ */
+
+/**
+ * Array constants for the different occupations
+ */
+export const occupations = ["Singer", "Politician", "Comedian", "Influencer"];
