5120a977382baac2d3889971e1d8fabc3feb6274
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-08-04 22:16:58 -0400

    Added topLevelAwait to true

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
-  }
-}
+    domains: ["res.cloudinary.com"],
+  },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
+  },
+};
 
 module.exports = nextConfig
