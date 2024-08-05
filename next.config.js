cfdaae2f7ea6adac845fdfee2695e7a4a2d42c47
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-08-04 22:28:00 -0400

    add the connectDB inside the route

--- next.config.js
+++ next.config.js
@@ -2,15 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"],
-  },
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config;
-  },
-};
+    domains: ['res.cloudinary.com'],
+  }
+}
 
 module.exports = nextConfig
--- pages/api/stars.ts
+++ pages/api/stars.ts
@@ -3,25 +3,25 @@ import connectDb from "@/lib/mongo/dbConnect";
 import MStar from "@/lib/mongo/models/star";
 import { IStar } from "@/utils/interfaces";
 
-await connectDb();
-
 interface Data {
-    success: boolean;
-    data?: IStar[];
-};
+  success: boolean;
+  data?: IStar[];
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    try {
-        const stars = await  MStar.find();
-        if (!stars) throw new Error("No Stars Found");
-        return res.status(200).json({ success: true, data: stars });
-      } catch (error) {
-          console.log(`Error in getting all stars : ${error}`);
-          return res.status(400).json({
-            success: false,
-          })
-      }
+  await connectDb();
+
+  try {
+    const stars = await MStar.find();
+    if (!stars) throw new Error("No Stars Found");
+    return res.status(200).json({ success: true, data: stars });
+  } catch (error) {
+    console.log(`Error in getting all stars : ${error}`);
+    return res.status(400).json({
+      success: false,
+    });
+  }
 }
