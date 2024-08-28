3026665e17b9afec83e6800c2f66a8fd35c5f3a4
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2024-08-27 23:57:39 -0400

    Fixed searchInput issue

--- components/searchInput.tsx
+++ components/searchInput.tsx
@@ -10,7 +10,7 @@ export default function SearchInput() {
     // If search term  is empty, don't fetch
     if (searchTerm === "") return;
 
-    fetch("/api/search/${searchTerm}")
+    fetch(`/api/search/${searchTerm}`)
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
